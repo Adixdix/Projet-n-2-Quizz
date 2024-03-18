7d22ddae4a4685a9397d7b13455940b5a97f5e7d
Pidgeyy <148491500+Pidgeyy@users.noreply.github.com>
2024-03-18 11:37:06 +0100

    Update game.puy
    Added the first structure

--- game.py
+++ game.py
@@ -1,1 +1,15 @@
-print("hello world Josh")
+class Game():
+    def __init__ (game_on, player_response, question, reponse):
+        game_on = True
+        player_response = #TO DECIDE
+        question = #TO DECIDE
+        response = #TO DECIDE
+        
+    def get_question(self):
+        #TODO
+        
+    def get_response(self):
+        #TODO
+    
+    def run(self):
+        #TODO
