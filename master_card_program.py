e1dd47f7371e286892d16d5262e8432e25c579aa
Adixdix <adamhabbadi77@gmail.com>
2024-04-06 16:19:45 +0200

    class carte maitre et carte joueur

--- master_card_program.py
+++ master_card_program.py
@@ -1,6 +1,7 @@
 from microbit import display, Image, button_a, button_b, sleep
 import radio
 
+
 class Master_card:
     def __init__(self) -> None:
         self._palyer_liste = {}
@@ -9,24 +10,23 @@ class Master_card:
     def get_player(self): 
         radio.on()
         while True:
-            serial_number = radio.receive()
-            if serial_number != None :
+            serial_number_received = radio.receive()
+            if serial_number_received != None and len(serial_number_received) == 20 :
                 for index in range(len(self._palyer_liste)):
-                    if serial_number == self._palyer_liste[index]:
+                    if serial_number_received == self._palyer_liste["j"+str(index)]:
                         return None 
                     else:
-                        self.set_player_id(serial_number,str(len(self._palyer_liste) + 1 ))
-                        radio.off()
+                        self.set_player_id(serial_number_received,str(len(self._palyer_liste) + 1 ))
                         break
 
-    def set_player_id(self,serial_number:int,id:str): 
-        self._palyer_liste ["j" + id] = serial_number
+    def set_player_id(self,serial_number_received:int,id:str): 
+        self._palyer_liste ["j" + id] = serial_number_received
         radio.on()
         while True:
-            radio.send(serial_number "j"+str(id))
+            radio.send(str(serial_number_received) "j"+str(id))
             sleep(50)
-            answer = radio.receive()
-            if answer == "ok":
+            answer_send = radio.receive()
+            if answer_send == "ok":
                 break
             sleep(50)
 
--- player_card_program.py
+++ player_card_program.py
@@ -1,6 +1,7 @@
-import random
 from microbit import display, Image, button_a, button_b, sleep
 import radio
+import random
+
 
 def get_serial_number(size = 20):
     set_serial_number = ""
@@ -13,11 +14,11 @@ def teste ():
     radio.on()
     serial_number = get_serial_number()
     while True:
-        radio.send()
-        sleep(50)
+        radio.send(str(serial_number))
         id = radio.receive()
         if id != None and len(id) == 23:
             for index in range(20):
                 serial_number_send = serial_number_send + id[index]
             if serial_number == serial_number_send:
                 id_player = "j"+str(id[23])
+                print(id_player)
