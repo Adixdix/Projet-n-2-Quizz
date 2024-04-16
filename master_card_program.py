60655ee6873796a2b313f1a8577ef56191497610
Adixdix <adamhabbadi77@gmail.com>
2024-04-16 21:27:36 +0200

    amélioration

--- master_card_program.py
+++ master_card_program.py
@@ -5,6 +5,7 @@ import radio
 class Master_card:
     def __init__(self) -> None:
         self._palyer_liste = {}
+        self.player_answer_list = {}
 
 
     def get_player(self): 
@@ -20,11 +21,11 @@ class Master_card:
                         break
 
 
-    def set_player_id(self,serial_number_received:int,id:str): 
+    def set_player_id(self,serial_number_received : int ,id : str): 
         self._palyer_liste ["j" + id] = serial_number_received
         radio.on()
         while True:
-            radio.send(str(serial_number_received) "j"+str(id))
+            radio.send(str(serial_number_received)+":"+"j"+str(id))
             sleep(50)
             answer_send = radio.receive()
             if answer_send == "ok":
@@ -33,15 +34,26 @@ class Master_card:
             sleep(50)
 
 
-    def get_response_mode(self):
+    def get_answer_mode(self):
         radio.on()
         for joueur in range(len(self._palyer_liste)):
-            radio.send("go reply"+str("j" + joueur))
+            radio.send("go reply"+":"+"j"+str(joueur))
         radio.off()
 
-    def response_recovery(self):
+
+    def get_answer(self,time_out : int) -> dict:
         radio.on()  
-        while True:
+        for _ in range(time_out):
             answer = radio.receive()
             if answer != None :
+                self.player_answer_list["j"+str(answer[1])] = answer[3]
+                radio.send("j"+str(answer[1])+":ok")
+                radio.off()
+
+    def get_player_answer_liste(self):
+        return self.player_answer_list
+    
+    def send_corrections_answer(self,player_answer_list_corrections):
+        for key in player_answer_list_corrections:
+            radio.send(str(player_answer_list_corrections[key])+":"+str(key))
                 
--- player_card_program.py
+++ player_card_program.py
@@ -2,28 +2,58 @@ from microbit import display, Image, button_a, button_b, sleep
 import radio
 import random
 
-
-def get_serial_number(size = 20):
-    set_serial_number = ""
-    for _ in range(size):
-        serial_number = set_serial_number + str(random.randint(0,9))
-        return(serial_number)
-
-def send_serial_number(serial_number):
-    radio.on()
-    while True:
-        radio.send(str(serial_number))
-        id = radio.receive()
-        if id != None and len(id) == 23:
-            for index in range(20):
-                serial_number_send = serial_number_send + id[index]
-            if serial_number == serial_number_send:
-                id_player = "j"+str(id[23])
-                return id_player
+class Player_card:
+    def __init__(self) -> None:
+        self.different_answer = ["A", "B", "C", "D"]
 
 
-def main(serial_number):
-    radio.on()
-    while True:
-        mode = radio.receive()
-        if mode  == "go reply"+str(serial_number)
+    def get_serial_number(self,size = 20):
+        set_serial_number = ""
+        for _ in range(size):
+            serial_number = set_serial_number + str(random.randint(0,9))
+            return(serial_number)
+
+
+    def send_serial_number(self,serial_number):
+        radio.on()
+        while True:
+            radio.send(str(serial_number))
+            id = radio.receive()
+            if id != None and len(id) == 23:
+                for index in range(20):
+                    serial_number_send = serial_number_send + id[index]
+                if serial_number == serial_number_send:
+                    id_player = "j"+str(id[23])
+                    return id_player
+
+
+    def waiting(self,id_player):
+        radio.on()
+        while True:
+            mode = radio.receive()
+            for index in range(8):
+                mode_ =  mode_ + mode[index]
+            if mode[len(mode)] == id_player[1] and str(mode_) == "go reply":
+                self.response_mode(self.different_answer)
+
+
+
+    def response_mode(self,different_answer,id_player):
+            index = 0
+            while True:
+                if button_a.was_pressed():
+                    display.show(different_answer[index % len(different_answer)])  
+                    index += 1
+                    index = index % 4
+                if button_b.was_pressed():
+                    display.show(Image.YES, wait=True)
+                    print(different_answer[index-1])#Attention a enlever a la fin 
+                    radio.on()
+                    while True:
+                        radio.send(str(different_answer[index-1])+":"+str(id_player)) 
+                        reception = radio.receive()
+                        for i in range(1):
+                            reception_ =  reception_ + reception[4 + i]
+                        if reception[len(reception)] == id_player[1] and  reception_ == "ok":
+                            radio.off()
+                            break
