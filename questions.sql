18a3cd349bd83caf31551e436ce88584af231c0f
LukasPyt <47983027+LukasPyt@users.noreply.github.com>
2024-04-22 07:53:42 +0200

    Add files via upload

---
+++ questions.sql
@@ -0,0 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.7.1
+-- http://www.phpmyadmin.net
+--
+-- Client :  localhost
+-- Généré le :  Lun 22 Avril 2024 à 05:53
+-- Version du serveur :  5.6.20-log
+-- Version de PHP :  5.4.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données :  `questions`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `questions`
+--
+
+CREATE TABLE IF NOT EXISTS `questions` (
+  `question_id` int(10) unsigned NOT NULL,
+  `question_label` varchar(100) NOT NULL,
+  `question_type` varchar(30) NOT NULL,
+  `difficulty` int(10) unsigned DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `questions`
+--
+
+INSERT INTO `questions` (`question_id`, `question_label`, `question_type`, `difficulty`) VALUES
+(1, 'Quelle est la capitale de la Turquie ?', 'Geographie', 2),
+(4, 'Quelle est la traduction de "loathsome" ?', 'Langues', 3),
+(3, 'Quelle est la valeur de x si 4x - 2 = 6 ?', 'Maths', 1),
+(2, 'Quelle est la taille de la Tour Eiffel ?', 'Histoire', 3);
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `questions`
+--
+ALTER TABLE `questions`
+ ADD PRIMARY KEY (`question_id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
