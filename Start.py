28f1a11a9c53d784060fd2aef81a8335f1644698
Pidgeyy <148491500+Pidgeyy@users.noreply.github.com>
2024-05-08 11:34:06 +0200

    Update Start.py
    added comments

--- Start.py
+++ Start.py
@@ -3,23 +3,16 @@ from tkinter import ttk, Toplevel
 from ttkbootstrap import Style
 from quizz_v3 import Game
 
-
-class New_window(Toplevel):
-     
-    def __init__(self, root=None):
-         
-        super().__init__(root = root)
-        self.title("New Window")
-        self.geometry("200x200")
-
         
 class Start():
     def __init__(self):
         self.root = tk.Tk()
+        self.style = Style(theme="flatly")
         self.window_list = []
         self.player_list = []
         self.quizz_label = ttk.Label(
             self.root,
+            text = "Quizz",
             anchor="center",
             wraplength=500,
             padding=10
@@ -32,44 +25,47 @@ class Start():
             )
         self.play_btn = ttk.Button(
             self.root,
+            text = "PLAY",
+            command = lambda: Game(self.root)
         )
         self.add_player = ttk.Button(
             self.root,
-            text="Add player",
-            command = lambda:self.choose_player(),
+            text="ADD PLAYER",
+            command = lambda: self.choose_player()
         )
     
-    def create_child():
-        child_window = tk.Toplevel(root)
-        child_window.title("Owned by Main Window")
-        child_window.transient(root)  # Set to be on top of the main window
-        tk.Label(child_window, text="I'm a child owned by the main window").pack()
-    
-    def choose_player(self):
+    def choose_player(self)->None:
+        """Add a single player to the game"""
         self.player_list.append(1)
         self.player_number.config(text=f"{len(self.player_list)} players")
-        
-    def run(self):
-        self.root.title("Quiz App")
-        self.root.geometry("600x500")
-        style = Style(theme="flatly")
-        self.root.resizable(height = True, width = True)
+    
+    def position(self)->None:
+        """Define the position of each label/button"""
         self.quizz_label.pack(pady=10)
         self.play_btn.pack(pady=5)
         self.add_player.pack(pady=5)
         self.player_number.pack(pady=5)
         
-        style.configure("TLabel", font=("Helvetica", 20))
-        style.configure("TButton", font=("Helvetica", 16))
+    def define_style(self)->None:
+        """Define the font of the writings"""
+        self.style.configure("TLabel", font=("Helvetica", 20))
+        self.style.configure("TButton", font=("Helvetica", 16))
+    
+    def window_paramaters(self)->None:
+        """Configure the main parameters of the window"""
+        self.root.title("Quiz App")
+        self.root.geometry("600x500")
+        self.root.resizable(height = True, width = True)
         
-        self.quizz_label.config(text="QUIZZ")
-        self.play_btn.config(text="PLAY")
-        self.add_player.config(text="ADD PLAYER")
+    def run(self)->None:
+        self.window_paramaters()
+        
+        self.position()
+        
+        self.define_style()
+        
         self.player_number.config(text=f"{len(self.player_list)} players")
         
-        #self.play_btn.bind("<Button>", 
-         #lambda x: Game(self.root))
-        self.play_btn = ttk.Button(self.root,command=self.create_child()).pack()
         self.root.mainloop()
         
         
