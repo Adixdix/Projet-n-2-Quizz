f57712139ca38cd405203da7017003295f1ecb03
Pidgeyy <148491500+Pidgeyy@users.noreply.github.com>
2024-05-08 14:57:51 +0200

    Update Start.py
    working with Database

--- Start.py
+++ Start.py
@@ -1,17 +1,7 @@
 import tkinter as tk
-from tkinter import ttk, Toplevel
+from tkinter import ttk, Menu
 from ttkbootstrap import Style
 from quizz_v3 import Game
-
-
-class New_window(Toplevel):
-     
-    def __init__(self, root=None):
-         
-        super().__init__(root = root)
-        self.title("New Window")
-        self.geometry("200x200")
-
         
 class Start():
     def __init__(self):
@@ -19,10 +9,11 @@ class Start():
         self.style = Style(theme="flatly")
         self.window_list = []
         self.player_list = []
+        self.nb_questions = 0 #default total questions
         self.quizz_label = ttk.Label(
-            self.root,
+            self.root, 
             text = "Quizz",
-            anchor="center",
+            anchor="center",  
             wraplength=500,
             padding=10
             )
@@ -32,10 +23,16 @@ class Start():
             wraplength=500,
             padding=10
             )
+        self.label_nb_questions = ttk.Label(
+            self.root,
+            anchor="center",
+            wraplength=500,
+            padding=10
+            )
         self.play_btn = ttk.Button(
             self.root,
             text = "PLAY",
-            command = lambda: Game(self.root).run()
+            command = lambda: Game(self.nb_questions, self.root).run()
         )
         self.add_player = ttk.Button(
             self.root,
@@ -54,6 +51,7 @@ class Start():
         self.play_btn.pack(pady=5)
         self.add_player.pack(pady=5)
         self.player_number.pack(pady=5)
+        self.label_nb_questions.pack(pady=5)
         
     def define_style(self)->None:
         """Define the font of the writings"""
@@ -65,6 +63,29 @@ class Start():
         self.root.title("Quiz App")
         self.root.geometry("600x500")
         self.root.resizable(height = True, width = True)
+    
+    def questions_modifier(self, nb_modifier:int)->None:
+        """Replace the total question numbers"""
+        self.nb_questions = nb_modifier
+        self.label_nb_questions.config(text=f"questions : {self.nb_questions}")
+        print("questions:", self.nb_questions)
+        
+    def menu_quizz(self)->None:
+        """Create the tab above the window"""
+        #create menu
+        menu_obj = Menu(self.root)
+        
+        #Add command to tab
+        questions_menu = Menu(menu_obj, tearoff=0)
+        
+        for i in range(10):
+            questions_menu.add_command(label =str(i+1), command =lambda i=i :self.questions_modifier(i+1))
+        
+        #Create each tab
+        menu_obj.add_cascade(label ="questions", menu=questions_menu)
+        
+        #update
+        self.root.config(menu = menu_obj)
         
     def run(self)->None:
         """Run the main menu"""
@@ -74,10 +95,12 @@ class Start():
         
         self.define_style()
         
+        self.menu_quizz()
+        
         self.player_number.config(text=f"{len(self.player_list)} players")
+        self.label_nb_questions.config(text=f"questions : {self.nb_questions}")
         
         self.root.mainloop()
-        
         
 test = Start()
 test.run()     
