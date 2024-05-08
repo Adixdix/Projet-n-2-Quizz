f3baa9e059da2a52a6a1e9575355812f031dc1ae
LukasPyt <47983027+LukasPyt@users.noreply.github.com>
2024-05-08 22:51:38 +0200

    Update questions.py

--- questions.py
+++ questions.py
@@ -2,14 +2,14 @@
 
 import pymysql.cursors
 from random import randint, sample
-from interrogation_BD import affiche_table
+# from interrogation_BD import affiche_table
 
 class Database():
     def __init__(self):
         # Will connect to the database each time this class is accessed
         self.connection = pymysql.connect(host='nsijoliotcurie.fr',
                                     user='admin_user_Quiz2024',
-                                    password='Uy0*9za59)C3',
+                                    password=####HIDDEN#####,
                                     database='admin_Quiz2024',
                                     charset='utf8mb4',
                                     cursorclass=pymysql.cursors.DictCursor)
@@ -30,14 +30,16 @@ class Database():
                 sql = "INSERT INTO questions VALUES (" + str(next_id) + ",'" + label + "','"  + question_type + "'," + str(difficulty) + ");"
                 cursor.execute(sql)
             self.connection.commit()
+            print('Successfully updated the QUESTIONS table.')
             with self.connection.cursor() as cursor:
                 # Adds the values in the "answers" table
                 sql = "INSERT INTO answers VALUES (" + str(next_id) + ",'" + correct_answer + "','" + wrong1 + "','"  + wrong2 + "','" + wrong3 + "');"
                 cursor.execute(sql)
             self.connection.commit()
+            print('Successfully updated the ANSWERS table.')
 
 
-    def get_question(self, question_id: int = None, difficulty: int = None, theme: int = None) -> dict:
+    def get_question(self, question_id: int = None, difficulty: int = '', theme: str = '') -> dict:
         '''Returns a question from the database (random if id not
         specified) in a dictionnary using the following structure :
         
@@ -74,28 +76,53 @@ class Database():
         return dictionnary
                            
 
-    def get_questions(self, n, difficulty=None, theme=None):
+    def get_questions(self, n: int, difficulty: int = '', theme:str = '') -> list:
         '''Returns n random questions from the database in a LIST of DICTIONNARIES
-        using the same structure as the 'get_question' function'''
+        using the same structure as the 'get_question' method'''
         list_of_questions = []
         with self.connection:
             with self.connection.cursor() as cursor:
+                
+                if difficulty != '':
+                    difficulty = "WHERE difficulty =" + str(difficulty)
+                if theme != '':
+                    if difficulty != '':
+                        theme = " AND question_type = '" + str(theme) + "'"
+                    else:
+                        theme = "WHERE question_type ='" + str(theme) + "'"
+                
                 # Counts the amount of ids stored in the database
-                sql = "SELECT COUNT(question_id) AS 'count' FROM questions"
+                sql = "SELECT COUNT(question_id) AS 'count' FROM questions " + difficulty + theme
                 cursor.execute(sql)
                 result = cursor.fetchone()['count']
-        if result < n:
-            # Raises an exception if the number of questions asked is greater than the number of questions in the database
-            raise ValueError('You asked for too many questions compared to the amount of ones available in the database')
-        # The following variable will basically contain a shuffled list of n integers that will refer to the questions' ids
-        list_ids = sample(list(range(1, result+1)), result)[:n]
-        # For each id in our shuffled list of ids, we call the previous function to fill our soon-to-be returned
-        # list with its label and answers
-        for element in list_ids:
-            list_of_questions.append(Database().get_question(element))
-        return list_of_questions
+                
+            
+            if difficulty == '' and theme == '':   
+                # The following variable will basically contain a shuffled list of n integers that will refer to the questions' ids
+                list_ids = sample(list(range(1, result+1)), result)[:n]
+            else:
+                with self.connection.cursor() as cursor:
+                    sql = "SELECT question_id FROM questions " + difficulty + theme
+                    cursor.execute(sql)
+                    list_ids = cursor.fetchall()
+                    for i in range(len(list_ids)):
+                        list_ids[i] = list_ids[i]['question_id']
+                    print(list_ids := sample(list_ids, len(list_ids))[:n])
+            if len(list_ids) < n:
+                # Raises an exception if the number of questions asked is greater than the number of questions in the database
+                raise ValueError('You asked for too many questions compared to the amount of ones available in the database')
+            # For each id in our shuffled list of ids, we call the previous method to fill our soon-to-be returned
+            # list with its label and answers
+            for element in list_ids:
+                list_of_questions.append(Database().get_question(element, difficulty, theme))
+            return list_of_questions
     
     
 # TESTS    
 if __name__ == '__main__':        
-    affiche_table(Database().get_questions(3))
+    for i in range(2):
+        affiche_table(Database().get_questions(5))
+        affiche_table(Database().get_questions(2, 2))
+        affiche_table(Database().get_questions(3, '', 'Histoire'))
+        affiche_table(Database().get_questions(1, 1, 'Maths'))
+    
